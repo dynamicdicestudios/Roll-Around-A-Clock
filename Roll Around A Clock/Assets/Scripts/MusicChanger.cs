8f1932918f67725d4ff8d04b840de6a8e6cc8434
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2020-10-19 09:11:16 -0400

    Create MusicChanger.cs

---
+++ Roll Around A Clock/Assets/Scripts/MusicChanger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicChanger : MonoBehaviour
+{
+    public string[] keys;
+	public AudioClip[] sounds;
+	
+	public AudioSource backgroundMusic;
+	
+	int sound;
+	
+    void Start()
+    {
+        for (int i = 0; i < keys.Length; i++) {
+			int state = PlayerPrefs.GetInt(keys[i]);
+			if (state == 1) {
+				sound = i;
+			}
+		}
+		
+		 backgroundMusic.clip = sounds[sound];
+		 backgroundMusic.Play();
+		
+    }
+}
