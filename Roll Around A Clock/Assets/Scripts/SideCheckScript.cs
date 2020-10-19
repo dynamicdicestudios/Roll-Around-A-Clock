418ea511943d82578db847dfa605f379085d2377
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2020-10-19 09:09:44 -0400

    Create SideCheckScript.cs

---
+++ Roll Around A Clock/Assets/Scripts/SideCheckScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideCheckScript : MonoBehaviour
+{
+    public DiceScript dice;
+	public int num = 0;
+	
+	Vector3 diceVelocity;
+	
+	void Start() {
+		diceVelocity = dice.diceVelocity;
+	}
+	
+	void OnTriggerStay(Collider col) {
+		if (diceVelocity.x == 0f && diceVelocity.y == 0f &&
+		diceVelocity.z == 0f) {
+			dice.isRolling = false;
+			if (col.gameObject.name == "DiceCheckZone")
+				dice.rolledNumber = num;
+		}
+	}
+}
