61c543da48c4ee1299e1d6af1ac3ae77d6c5ada0
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2020-10-19 09:10:33 -0400

    Create DiceNumberTextScript.cs

---
+++ Roll Around A Clock/Assets/Scripts/DiceNumberTextScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceNumberTextScript : MonoBehaviour
+{
+    Text text;
+	
+	public DiceScript diceA;
+	public DiceScript diceB;
+	public DiceScript diceC;
+	public static int diceNumber;
+	
+	public static bool finished;
+	
+	int numA, numB, numC, check;
+	
+	// Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		bool rollingA = diceA.isRolling;
+		bool rollingB = diceB.isRolling;
+		bool rollingC = diceC.isRolling;
+		
+		if (rollingA || rollingB || rollingC)
+			return;
+		
+		numA = diceA.rolledNumber;
+		numB = diceB.rolledNumber;
+		numC = diceC.rolledNumber;
+		
+        diceNumber =  numA + numB + numC;
+		/*if (diceNumber == check)
+			finished = true;
+		else
+			finished = false;*/
+		
+		check = diceNumber;
+		text.text = diceNumber.ToString();
+    }
+}
